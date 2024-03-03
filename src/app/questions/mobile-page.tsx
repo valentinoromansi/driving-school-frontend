48a32634aea14faca3698fd7d312a4891eb8812b
valentinoromansi <valentinoromansi@gmail.com>
2024-03-03 02:19:57 +0100

    search question feature added

--- src/app/questions/mobile-page.tsx
+++ src/app/questions/mobile-page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { InfiniteScroll } from '../common/infinite-scroll'
 import { Question } from '../model/model'
 import { AnswerGiven, AnswersGroup } from './answers-group'
-import { Card, CardContent, TextField, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from '@mui/material'
+import { Cancel, SearchOutlined, Visibility } from '@mui/icons-material'
+import { useForm } from 'react-hook-form';
 
 const answersGiven: AnswerGiven[] = [
   { id: 1, checked: true },
@@ -14,11 +16,52 @@ const answersGiven: AnswerGiven[] = [
   { id: 6, checked: true }
 ]
 
+type QuestionsFilterType = {
+  question: string
+}
+
 const MobilePage = () => {
+
+  const {
+    register,
+    getValues,
+    setValue,
+    watch,
+    resetField,
+
+  } = useForm<QuestionsFilterType>();
+
   return (
     <div style={{ width: '100%', height: '100vh', overflow: 'hidden'}}>
-      <TextField>
-      </TextField>
+      <Card sx={{ padding: '16px' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}>
+          <FormControl sx={{ flexGrow: 1 }}>
+            <InputLabel>Title</InputLabel>
+            <OutlinedInput
+              {...register("question")}
+              label="Title"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => { resetField('question') }}
+                  >
+                    {
+                      watch('question') && <Cancel/>
+                    } 
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <Box sx={{ flexShrink: 1 }}>
+            <Button sx={{ flexBasis: '100%', height: '100%'}} variant="contained" endIcon={<SearchOutlined />}>
+              Search
+            </Button>
+          </Box>
+        </Box>
+
+      </Card>      
       <InfiniteScroll<Question>
         itemWrapperSx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}
         pageSize={10}
@@ -45,9 +88,9 @@ const QuestionCard = ({ question, answersGiven }: QuestionCardProps) => {
         <CardContent>
           <Typography fontWeight={'bold'}>{question.text}</Typography>
           <br/>
-          {
-            question.resources?.map(resource => <img style={{ maxWidth: '100%', maxHeight: '12rem' }} src={'https://' + resource.uri}></img>)
-          }
+              {
+                question.resources?.map(resource => <img style={{ maxWidth: '100%', maxHeight: '12rem' }} src={'https://' + resource.uri}></img>)
+              }
           <AnswersGroup answers={question.answers} answersGiven={answersGiven}/>
         </CardContent>
       </Card>
